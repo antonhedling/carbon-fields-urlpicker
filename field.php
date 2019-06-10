30d0913ddfb73ad97a45dfe880fb7c9ac1d88947
Anton Hedling <anton.hedling@agilitet.se>
2019-06-10 18:08:53 +0200

    Remove add_action hook

--- field.php
+++ field.php
@@ -4,8 +4,6 @@ use Carbon_Field_UrlPicker\UrlPicker_Field;
 
 define('Carbon_Field_UrlPicker\\DIR', __DIR__);
 
-add_action('after_setup_theme', function () {
-	Carbon_Fields::extend(UrlPicker_Field::class, function ($container) {
-		return new UrlPicker_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
-	});
-}, 99);
+Carbon_Fields::extend(UrlPicker_Field::class, function ($container) {
+	return new UrlPicker_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
+});
